fff518c2b69de4124011c5e94d65914667927026
HongSena <red0sena@kakao.com>
2022-04-29 03:03:49 +0900

    -add listview data

--- lib/screens/start/address_service.dart
+++ lib/screens/start/address_service.dart
@@ -1,23 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:sports_matching/constants/keys.dart';
+import 'package:sports_matching/data/AddressModle.dart';
 import 'package:sports_matching/utils/logger.dart';
 class AddressService{
-  void searchAddresBystr(String text) async {
-
-    final formData = {
-      'key:': VWORLD_KEY,
-      'request': 'search',
-      'type': 'ADDRESS',
-      'category': "ROAD",
-      'query': text,
-      'size': 30,
-    };
-    logger.d(formData);
+  Future<AddressModle> searchAddresBystr(String text) async {
     final response = await Dio()
-        .get('http://api.vworld.kr/req/search', queryParameters: formData)
+        .get('http://api.vworld.kr/req/search?request=search&size=30&query=${text}&type=address&category=road&key=${VWORLD_KEY}')
         .catchError((e){
       logger.e(e.message);
     });
     logger.d(response);
+    AddressModle addressModle = AddressModle.fromJson(response.data["response"]);
+    logger.d(addressModle);
+    return addressModle;
+
   }
 }
+
