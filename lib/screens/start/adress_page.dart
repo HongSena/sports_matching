4c450c031101564e3c4459b2e15aae26d1401c05
HongSena <red0sena@kakao.com>
2022-04-29 16:16:45 +0900

    -add LocationInfo

--- lib/screens/start/adress_page.dart
+++ lib/screens/start/adress_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:sports_matching/data/AddressModle.dart';
 import 'package:sports_matching/screens/start/address_service.dart';
 import 'package:sports_matching/utils/logger.dart';
 
-class AdressPage extends StatelessWidget {
+class AdressPage extends StatefulWidget {
   AdressPage({Key? key}) : super(key: key);
 
+  @override
+  State<AdressPage> createState() => _AdressPageState();
+}
+
+class _AdressPageState extends State<AdressPage> {
   TextEditingController _addressController = TextEditingController();
+
+  AddressModle? _addressModel;
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +26,12 @@ class AdressPage extends StatelessWidget {
         children: [
         TextFormField(
           controller: _addressController,
+          onFieldSubmitted: (text) async {
+            _addressModel = await AddressService().searchAddresBystr(text);
+            setState(() {
+
+            });
+          },
           decoration: InputDecoration(
               prefixIcon: Icon(Icons.search, color: Colors.grey,),
           hintText: '도로명주소를 입력해주세요',
@@ -25,11 +40,31 @@ class AdressPage extends StatelessWidget {
             borderSide: BorderSide(color: Colors.grey))
           ),
         ),
-        TextButton.icon(onPressed: (){
-          final text = _addressController.text;
-          if(text.isNotEmpty){
-            AddressService().searchAddresBystr(text);
+        TextButton.icon(onPressed: () async {
+
+          Location location = new Location();
+
+          bool _serviceEnabled;
+          PermissionStatus _permissionGranted;
+          LocationData _locationData;
+
+          _serviceEnabled = await location.serviceEnabled();
+          if (!_serviceEnabled) {
+            _serviceEnabled = await location.requestService();
+            if (!_serviceEnabled) {
+              return;
+            }
           }
+
+          _permissionGranted = await location.hasPermission();
+          if (_permissionGranted == PermissionStatus.denied) {
+            _permissionGranted = await location.requestPermission();
+            if (_permissionGranted != PermissionStatus.granted) {
+              return;
+            }
+          }
+          _locationData = await location.getLocation();
+          logger.d(_locationData);
         },
             icon:Icon(
               CupertinoIcons.compass,
@@ -46,12 +81,15 @@ class AdressPage extends StatelessWidget {
           child: ListView.builder(
             padding: EdgeInsets.symmetric(vertical: 16),
             itemBuilder: (context, index){
-            logger.d('index: $index');
+              if (_addressModel==null || _addressModel!.result==null || _addressModel!.result!.items == null || _addressModel!.result!.items![index].address == null){
+                return Container();
+              }
             return ListTile(
-              title: Text('adress : $index'),
-              subtitle: Text('subtitle : $index'),
+              title: Text(_addressModel!.result!.items![index].address!.road!),
+              subtitle: Text(_addressModel!.result!.items![index].address!.parcel!),
             );
-          }, itemCount: 30,),
+          },
+            itemCount: (_addressModel==null || _addressModel!.result==null || _addressModel!.result!.items == null )? 0: _addressModel!.result!.items!.length,),
         )
       ],),
     );
