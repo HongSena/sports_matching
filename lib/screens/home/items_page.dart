9ae573c16807712ec210f53d3cb68a46094ed03b
HongSena <red0sena@kakao.com>
2022-05-05 15:23:36 +0900

    -add firebase android

--- lib/screens/home/items_page.dart
+++ lib/screens/home/items_page.dart
@@ -8,43 +8,46 @@ class ItemsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: EdgeInsets.all(common_small_padding),
-      separatorBuilder: (context, index){
-        return Divider(
-          height: common_padding*2+1,
-          thickness: 1,
-          color: Colors.grey.shade300,
-          indent: common_small_padding,
-        );
-      },
-      itemBuilder: (context, index) {
-      return SizedBox(
-        height: 100,
-        child: Row(
-          children: [
-            ExtendedImage.asset('assets/imgs/tomato.png'),
-            SizedBox(width: common_small_padding,),
-            Expanded(child: Column( crossAxisAlignment: CrossAxisAlignment.start,
+    return LayoutBuilder(
+      builder: (context, constraints){
+        Size size = MediaQuery.of(context).size;
+        final imgSize = size.width*0.25;
+        return ListView.separated(
+          padding: EdgeInsets.all(common_small_padding),
+          separatorBuilder: (context, index){
+            return Divider(
+              height: common_padding*2+1,
+              thickness: 1,
+              color: Colors.grey.shade300,
+              indent: common_small_padding,
+            );
+          },
+          itemBuilder: (context, index) {
+            return SizedBox(height: imgSize, child: Row(
               children: [
-                Text('work', style: Theme.of(context).textTheme.subtitle1),
-                Text('53일 전', style: Theme.of(context).textTheme.subtitle2),
-                Text('5000원'),
-                Expanded(child: Container()),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(CupertinoIcons.chat_bubble_2, color: Colors.grey[500], size: 17),
-                  Text('23', style: TextStyle( color: Colors.grey[500], fontSize: 13)),
-                  Icon(CupertinoIcons.heart, color: Colors.grey[500], size: 17),
-                  Text('30', style: TextStyle( color: Colors.grey[500], fontSize: 13),
-                  )],
-              )],
-            ))
-          ],
-        ),
-      );
-    }, itemCount: 10,);
+                SizedBox(height: imgSize, width: imgSize, child: ExtendedImage.asset('assets/imgs/ronnie.png', shape: BoxShape.rectangle ,borderRadius: BorderRadius.circular(12),)),
+                SizedBox(width: common_small_padding,),
+                Expanded(child: Column( crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('work', style: Theme.of(context).textTheme.subtitle1),
+                    Text('53일 전', style: Theme.of(context).textTheme.subtitle2),
+                    Text('5000원'),
+                    Expanded(child: Container()),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(CupertinoIcons.chat_bubble_2, color: Colors.grey[500], size: 17),
+                        Text('23', style: TextStyle( color: Colors.grey[500], fontSize: 13)),
+                        Icon(CupertinoIcons.heart, color: Colors.grey[500], size: 17),
+                        Text('30', style: TextStyle( color: Colors.grey[500], fontSize: 13),)
+                      ],
+                    )
+                  ],
+                ))
+              ],
+            ),);}, itemCount: 10,);
+      }
+    );
   }
 }
 
